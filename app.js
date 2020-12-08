cf81c984c77623b79658e3ef7d01099550d7ee94
Kosta Mitanovski <mkosta@amazon.com>
2020-12-08 16:59:00 +1100

    another change made

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to my AWS CI/CD!'));
+app.get('/', (req, res) => res.send('This is another change in the repo'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
